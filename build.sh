c53387b886a2c7ac20e63ae758410ce2f55aad0d
Akihiro Motoki <amotoki@gmail.com>
2018-05-29 12:46:38 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ fi
 
 LANG=$1
 
+sphinx-build -W -b gettext -d doc/build/doctrees.gettext doc/source doc/source/locale
 msgcat --no-wrap doc/source/locale/$LANG/LC_MESSAGES/doc.po | sed -f ./adjust.sed > doc/source/locale/$LANG/LC_MESSAGES/index.po
-sphinx-build -a -W -b html -D language=$LANG doc/source doc/build/html/$LANG
+sphinx-build -a -W -b html -D language=$LANG -d doc/build/doctrees.$LANG doc/source doc/build/html/$LANG
