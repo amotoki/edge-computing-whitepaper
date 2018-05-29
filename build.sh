328c78e80e3b3c0ad041ec98834045bd32e455fc
Akihiro Motoki <amotoki@gmail.com>
2018-05-29 12:42:50 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 if [ $# -lt 1 ]; then
   echo "Usage: $0 <lang>"
 fi
@@ -7,3 +9,4 @@ fi
 LANG=$1
 
 msgcat --no-wrap doc/source/locale/$LANG/LC_MESSAGES/doc.po | sed -f ./adjust.sed > doc/source/locale/$LANG/LC_MESSAGES/index.po
+sphinx-build -a -W -b html -D language=$LANG doc/source doc/build/html/$LANG
